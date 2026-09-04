dcea08102520e3e1a43b7d5980595818ccb122d1
aaronbedi <jajnsbsjdj@gmail.com>
2026-09-04 18:14:02 +0800

    Add files via upload

---
+++ print.py
@@ -0,0 +1,8 @@
+age=10
+score=67.67
+city="perth"
+name="Aaron"
+print("my name is ",name)
+print("I live in ",city)
+print ("I am ",age,"years old.")
+print ("My score from the last class was: ",score)
